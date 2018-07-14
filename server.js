41e29ea01d66772a8ffb97ff7902cd165454a35d
shayanbagchi <bagchishayan.9@gmail.com>
2018-07-14 13:53:20 +0530

    adding new files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res) => {res.send(database.users)})
+app.post('/signin', (req,res) => {signin.handleSignin(req, res)} )
+app.post('/register', (req,res) => {register.handleRegister(req, res)} )
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res)} )
+app.put('/image', (req,res) => {image.handleImage(req, res)} )
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)} )
+app.listen(3001,() =>{
+	console.log('app is running on port 3001');
+});
