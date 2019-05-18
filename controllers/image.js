7f08a79326271e2cdfed78d892f3727d4ee57de2
shayanbagchi <bagchishayan.9@gmail.com.com>
2019-05-18 21:06:09 +0530

    Update 0.1.3

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require ('clarifai');
 
 const app = new Clarifai.App({
- apiKey: process.env.API_KEY
+ apiKey: process.env.Clarifai_API_KEY
 });
 
 const handleApiCall = (req,res) =>{
