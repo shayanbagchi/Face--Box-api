56677aaa549e26c748829ac315194da2cebcf603
shayanbagchi <bagchishayan.9@gmail.com.com>
2019-05-18 20:05:56 +0530

    Update 0.1

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res)} )
 app.put('/image', (req,res) => {image.handleImage(req, res)} )
 app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)} )
 app.listen(process.env.PORT || 3001,() =>{
-	console.log(`app is running on port ${process.env.PORT}`);
+	console.log(`app is running on port ${process.env.PORT || 3001}`);
 });
